e0d5d1734b93e5f57e90ef354f8ff0139d48a06b
Lincoln Dias Marques <licodmarques@gmail.com>
2022-07-28 01:31:37 -0300

    fix: vercel config file

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,7 @@
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
-
-Sentry.init({
-  dsn: "https://8f58dbb739de4fb19c1951217ba3c685@o1337392.ingest.sentry.io/6607140",
-  integrations: [new BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
