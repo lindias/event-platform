30a2d7749ad0df2f4c5cf7ef506d0c9c5ca859b4
Lincoln Dias Marques <licodmarques@gmail.com>
2022-07-28 01:09:25 -0300

    fix: react router 2nd

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { Event } from "./pages/Event";
+import { NotFound } from "./pages/NotFound";
 import { Subscribe } from "./pages/Subscribe";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Subscribe />} />
-      <Route path="event" element={<Event />} />
-      <Route path="event/lesson/:slug" element={<Event />} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
